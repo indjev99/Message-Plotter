5bc58df62ab26d4d0a51f6706ccafdf0e1433a46
Emil Indzhev <emil.indjev@gmail.com>
2019-12-19 12:03:11 +0200

    Updated the message finder
    Currently arguments are set in the code

--- message-finder.py
+++ message-finder.py
@@ -5,7 +5,7 @@ import datetime
 import sys
 import matplotlib.pyplot as plt
 
-chat_root = 'messages/inbox'
+chat_root = os.path.join('messages', 'inbox')
 month_names = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
 def fix(s):
@@ -22,30 +22,37 @@ def count_chars(msg):
 def date_diff(f, t):
     return (t - f).days
 
+def format(n):
+    return '{:02d}'.format(n)
+
 # my_name - your name on Facebook
-def load_data(my_name):
-
+def print_messages(my_name, target_name, start_date, end_date):
     for f in os.listdir(chat_root):
-        file = os.path.join(chat_root, f, 'message_1.json')
-        try:
-            data = json.load(open(file, 'r'))
-        except FileNotFoundError:
-            continue
+        all_messages = []
+        for num in range(1000):
+            msg_name = 'message_' + str(num + 1) + '.json'
+            file = os.path.join(chat_root, f, msg_name)
+            try:
+                data = json.load(open(file, 'r'))
+            except FileNotFoundError:
+                break
 
-        if data['thread_type'] != 'Regular':
-            continue
+            if num == 0:
+                if data['thread_type'] != 'Regular':
+                    break
+                try:
+                    assert(len(data['participants']) == 2 and fix(data['participants'][1]['name']) == my_name)
+                except AssertionError:
+                    break
+                partner_name = fix(data['participants'][0]['name'])
+                if partner_name == my_name:
+                    break
+                if partner_name != target_name:
+                    break
 
-        try:
-            assert(len(data['participants']) == 2 and fix(data['participants'][1]['name']) == my_name)
-        except AssertionError:
-            continue
+            all_messages += data['messages']
 
-        partner_name = fix(data['participants'][0]['name'])
-
-        if partner_name != target_name:
-            continue
-
-        for msg in reversed(data['messages']):
+        for msg in reversed(all_messages):
             date = datetime.date.fromtimestamp(msg['timestamp_ms'] / 1000)
             dtime = datetime.datetime.fromtimestamp(msg['timestamp_ms'] / 1000)
             if start_date <= date and date <= end_date:
@@ -54,14 +61,14 @@ def load_data(my_name):
                     text = fix(msg['content'])
                 except KeyError:
                     text = 'Sticker, file, etc.'
-                print(str(date.day) + '-' + month_names[date.month-1] + '-' + str(date.year) + ' ' + str(dtime.hour) + ':' + str(dtime.minute) + ':' + str(dtime.second))
+                print(format(date.day) + '-' + month_names[date.month-1] + '-' + str(date.year) + ' ' + format(dtime.hour) + ':' + format(dtime.minute) + ':' + format(dtime.second))
                 print(sender_name + ': ' + text)
 
-# Arguments: 
+# Arguments:
 my_name = ''
 target_name = ''
 start_date = datetime.date(2018, 4, 4)
 end_date = datetime.date(2018, 4, 4)
 
-# my_name = input('Enter your name on Facebook: ')
-load_data(my_name)
+if __name__ == '__main__':
+    print_messages(my_name, target_name, start_date, end_date)
